94f97fab8ef23ba286362ee6dc940c2a1e73b99b
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-11 01:03:57 +0530

    introduce footer and fix import consistency

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,9 +3,10 @@
 import { fabric } from "fabric";
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { useEffect, useRef } from "react";
-import { Navbar } from "./navbar";
-import { Sidebar } from "./sidebar";
-import { Toolbar } from "./toolbar";
+import { Navbar } from "@/features/editor/components/navbar";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
+import { Footer } from "@/features/editor/components/footer";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -36,9 +37,13 @@ export const Editor = () => {
         <Sidebar />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
-          <div className="flex-1 h-full bg-muted" ref={containerRef}>
+          <div
+            className="flex-1 h-[calc(100%-124px)] bg-muted"
+            ref={containerRef}
+          >
             <canvas ref={canvasRef} />
           </div>
+          <Footer />
         </main>
       </div>
     </div>
