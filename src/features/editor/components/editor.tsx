6955f6f1cf712dd3f4f1246436019b6482500bb4
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-11 00:45:39 +0530

    introduce navbar

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,6 +3,7 @@
 import { fabric } from "fabric";
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { useEffect, useRef } from "react";
+import { Navbar } from "./navbar";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -24,8 +25,11 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvasRef} />
+      <Navbar />
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <div className="flex-1 h-full bg-muted" ref={containerRef}>
+          <canvas ref={canvasRef} />
+        </div>
       </div>
     </div>
   );
