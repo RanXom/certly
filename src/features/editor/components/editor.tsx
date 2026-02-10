98f87d2cfbbdc93acf47b41d06c003599189c238
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-11 00:58:38 +0530

    introduce sidebar and toolbar layout

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -4,6 +4,8 @@ import { fabric } from "fabric";
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { useEffect, useRef } from "react";
 import { Navbar } from "./navbar";
+import { Sidebar } from "./sidebar";
+import { Toolbar } from "./toolbar";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -21,15 +23,23 @@ export const Editor = () => {
       initialCanvas: canvas,
       initialContainer: containerRef.current!,
     });
+
+    return () => {
+      canvas.dispose();
+    };
   }, [init]);
 
   return (
     <div className="h-full flex flex-col">
       <Navbar />
       <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
-        <div className="flex-1 h-full bg-muted" ref={containerRef}>
-          <canvas ref={canvasRef} />
-        </div>
+        <Sidebar />
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+          <div className="flex-1 h-full bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef} />
+          </div>
+        </main>
       </div>
     </div>
   );
