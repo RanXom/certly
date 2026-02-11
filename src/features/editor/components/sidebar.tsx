c7cbfb6f1402273123954e59858613d5f90c87cb
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-11 19:14:48 +0530

    add sidebar elements

--- src/features/editor/components/sidebar.tsx
+++ src/features/editor/components/sidebar.tsx
@@ -1,7 +1,51 @@
 "use client";
 
+import {
+  LayoutTemplate,
+  ImageIcon,
+  Pencil,
+  Presentation,
+  Settings,
+  Shapes,
+  Type,
+} from "lucide-react";
+import { SidebarItem } from "./sidebar-items";
+
 export const Sidebar = () => {
   return (
-    <aside className="bg-white flex flex-col w-[100px] h-full border-r overflow-y-auto"></aside>
+    <aside className="bg-white flex flex-col w-[100px] h-full border-r overflow-y-auto">
+      <ul className="flex flex-col">
+        <SidebarItem
+          icon={LayoutTemplate}
+          label="Design"
+          isActive={false}
+          onClick={() => {}} //TODO
+        />
+        <SidebarItem
+          icon={ImageIcon}
+          label="Image"
+          isActive={false}
+          onClick={() => {}} //TODO
+        />
+        <SidebarItem
+          icon={Type}
+          label="Text"
+          isActive={false}
+          onClick={() => {}} //TODO
+        />
+        <SidebarItem
+          icon={Shapes}
+          label="Shapes"
+          isActive={false}
+          onClick={() => {}} //TODO
+        />
+        <SidebarItem
+          icon={Settings}
+          label="Settings"
+          isActive={false}
+          onClick={() => {}} //TODO
+        />
+      </ul>
+    </aside>
   );
 };
