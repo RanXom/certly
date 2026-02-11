596c947c41a70651937259ab1ab049cb0da9a5ed
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-12 00:48:08 +0530

    style the sidebar close button

--- src/features/editor/components/tool-sidebar-close.tsx
+++ src/features/editor/components/tool-sidebar-close.tsx
@@ -6,7 +6,10 @@ interface ToolSidebarCloseProps {
 
 export const ToolSidebarClose = ({ onClick }: ToolSidebarCloseProps) => {
   return (
-    <button onClick={onClick}>
+    <button 
+        onClick={onClick}
+        className="absolute -right-[1.80rem] h-[70px] bg-white top-1/2 transform -translate-y-1/2 flex items-center justify-center rounded-r-xl px-1 pr-2 border-r border-y group"
+    >
       <ChevronsLeft className="size-4 text-black group-hover:opacity-75 transition" />
     </button>
   );
