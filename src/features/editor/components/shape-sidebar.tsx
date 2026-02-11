6e1078ce87e3ff19f548051eaf3ecd4c6455cdf3
Saiyed Shizain <saiyedshizain@gmail.com>
2026-02-12 01:04:15 +0530

    add shapes to shapes sidebar

--- src/features/editor/components/shape-sidebar.tsx
+++ src/features/editor/components/shape-sidebar.tsx
@@ -1,7 +1,15 @@
+import { IoTriangle } from "react-icons/io5";
+import { FaDiamond } from "react-icons/fa6";
+import { FaCircle, FaSquare, FaSquareFull } from "react-icons/fa";
+
 import { ActiveTool } from "@/features/editor/types";
-import { cn } from "@/lib/utils";
 import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
-import { ToolSidebarClose } from "./tool-sidebar-close";
+import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
+import { ShapeTool } from "@/features/editor/components/shape-tool";
+
+import { cn } from "@/lib/utils";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ShapeSidebarProps {
   activeTool: ActiveTool;
@@ -27,6 +35,20 @@ export const ShapeSidebar = ({
         title="Shapes"
         description="Add shapes to your canvas"
       />
+      <ScrollArea>
+        <div className="grid grid-cols-3 gap-4 p-4">
+          <ShapeTool onClick={() => {}} icon={FaCircle} />
+          <ShapeTool onClick={() => {}} icon={FaSquare} />
+          <ShapeTool onClick={() => {}} icon={FaSquareFull} />
+          <ShapeTool onClick={() => {}} icon={IoTriangle} />
+          <ShapeTool
+            onClick={() => {}}
+            icon={IoTriangle}
+            iconClassName="rotate-180"
+          />
+          <ShapeTool onClick={() => {}} icon={FaDiamond} />
+        </div>
+      </ScrollArea>
       <ToolSidebarClose onClick={onClose} />
     </aside>
   );
