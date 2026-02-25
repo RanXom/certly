2733052ace490ebf69b74acbc7f36f7a07dacf08
RanXom <saiyedshizain@gmail.com>
2026-02-25 15:16:34 +0530

    add reset option in zoom

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -74,7 +74,7 @@ const buildEditor = ({
 
       canvas.zoomToPoint(
         new fabric.Point(center.left, center.top),
-        zoomRatio
+        zoomRatio > 1 ? 1 : zoomRatio
       );
     },
     zoomOut: () => {
