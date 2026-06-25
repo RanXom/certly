a17b86a58fc1dfee1a0d509d465b6035ced38eba
RanXom <saiyedshizain@gmail.com>
2026-06-25 21:59:21 +0530

    add protectServer function to stop user from accessing editor unless logged in

---
+++ src/app/editor/[projectId]/editor-client.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+// Forcing CSR in order to avoid missing jsdom runtime error
+const Editor = dynamic(
+  () => import("@/features/editor/components/editor").then((mod) => mod.Editor),
+  { ssr: false },
+);
+
+export const EditorClient = () => {
+  return <Editor />;
+};
