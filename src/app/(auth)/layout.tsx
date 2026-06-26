de9d9b21a6d0cc763440eef248f0a93c589b4f05
RanXom <saiyedshizain@gmail.com>
2026-06-26 23:13:00 +0530

    add bg to auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="bg-[url(/bg.png)] bg-top bg-cover h-full flex items-center justify-center">
       <div className="h-full w-full md:h-auto md:w-105">{children}</div>
     </div>
   );
