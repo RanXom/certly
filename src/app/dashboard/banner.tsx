fcd93594b4d28a60719fd5e6149cb24ffbc1042e
RanXom <saiyedshizain@gmail.com>
2026-07-03 14:48:34 +0530

    introduce project schema and save project into db

--- src/app/dashboard/banner.tsx
+++ src/app/dashboard/banner.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Sparkles } from "lucide-react";
+import { useCreateProject } from "@/features/projects/use-create-project";
+import { useRouter } from "next/navigation";
 
 export const Banner = () => {
+  const router = useRouter();
+  const mutation = useCreateProject();
+
+  const onClick = () => {
+    mutation.mutate(
+      {
+        name: "Untitled Project",
+        json: "",
+        height: 900,
+        width: 1200,
+      },
+      {
+        onSuccess: ({ data }) => {
+          router.push(`/editor/${data.id}`);
+        },
+      },
+    );
+  };
+
   return (
     <div className="text-white aspect-5/1 min-h-62 flex gap-x-6 p-6 items-center rounded-xl bg-linear-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
       <div className="rounded-full size-28 flex items-center justify-center bg-white/50 hidden md:flex">
@@ -18,7 +41,12 @@ export const Banner = () => {
           hundreds of personalized designs effortlessly. No design skills
           required.
         </p>
-        <Button variant="secondary" className="w-40">
+        <Button
+          disabled={mutation.isPending}
+          onClick={onClick}
+          variant="secondary"
+          className="w-40"
+        >
           Start Creating
           <ArrowRight className="size-4 ml-1" />
         </Button>
